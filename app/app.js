1c33545fe7e353a5b465b23e46313fe6ef4f4cb2
veera sekaran PL <veeramanickam2000@gmail.com>
2026-03-16 19:03:05 +0530

    Version 2 - Rolling update, scaling and self-healing demo

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-res.write("DevOps CI/CD Kubernetes Project")
+res.write("DevOps CI/CD Kubernetes Project - Version 2")
 res.end()
 })
 
